3193055e5f238c8c74054bdf417274972aa62296
Omer Arad <omerar@wix.com>
2024-05-04 21:31:42 +0300

    remove hom folder

--- app/page.tsx
+++ app/page.tsx
@@ -8,21 +8,16 @@ import Toolbar from "./ui/toolbar/toolbar";
 export default function Home() {
   return (
     <>
-      <Toolbar />
-      <div className="flex h-full flex-col md:flex-row md:overflow-hidden">
-        <div className="flex-grow p-6 md:overflow-y-auto md:px-0 pb-0">
-          <div className="flex md:flex-row flex-col">
-            <main className="flex flex-1 flex-col h-full items-strech md:w-2/3">
-              <section className="flex flex-col justify-between md:flex-row">
-                <SearchSection />
-                <RecommendationsSection />
-              </section>
-              <TrendingMoviesSection />
-              <TrendingTvSection />
-            </main>
-            <CollapsibleFeed />
-          </div>
-        </div>
+      <div className="flex md:flex-row flex-col">
+        <main className="flex flex-1 flex-col h-full items-strech md:w-2/3">
+          <section className="flex flex-col justify-between md:flex-row">
+            <SearchSection />
+            <RecommendationsSection />
+          </section>
+          <TrendingMoviesSection />
+          <TrendingTvSection />
+        </main>
+        <CollapsibleFeed />
       </div>
     </>
   );
