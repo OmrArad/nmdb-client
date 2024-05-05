716a57430a53822d2aa11b9d23440db493673882
Omer Arad <omerar@wix.com>
2024-05-05 18:51:10 +0300

    user dashboard

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 import React from "react";
+import UserDashboard from "../ui/dashboard/userDashboard";
 
 const UserComponent = () => {
-  return <>User Dashboard</>;
+  return (
+    <div>
+      <UserDashboard />
+      <>User Dashboard</>
+    </div>
+  );
 };
 
 export default UserComponent;
