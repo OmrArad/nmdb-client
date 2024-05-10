c9f482d7d9667ab547865563bea7f15464ea220f
Omer Arad <omerar@wix.com>
2024-05-11 01:08:03 +0300

    get movie from static json data, needs streaming, missing data

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,34 +1,48 @@
+"use client";
+import type { DetailedMovie } from "@/app/types/movie";
+import movieObject from "@/app/data/movies.json";
 import React from "react";
+import axios from "axios";
+import Image from "next/image";
 
-const MoviePage = () => {
-  // Mock data or you can fetch real data from an API
-  const movieData = {
-    title: "Movie Title",
-    genres: ["Genre 1", "Genre 2"],
-    duration: "97 minutes",
-    releaseYear: "2005",
-    overview:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry...",
-    director: "Director name",
-    screenplay: "Screenwriter",
-    rating: 7.9,
-    reviewersQuote:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-    streamingServices: ["NETFLIX", "prime video"], // You can add logos or icons for each service
-    // ... other movie data
-  };
+type MoviePageProps = {
+  id: number;
+};
+
+const MoviePage: React.FC<MoviePageProps> = ({ id }) => {
+  const urlPrefix = "https://image.tmdb.org/t/p/w220_and_h330_face";
+  const urlPrefixOriginal = "https://image.tmdb.org/t/p/original";
+
+  const [movie, setMovie] = React.useState<DetailedMovie>(movieObject[0]);
+
+  React.useEffect(() => {
+    axios
+      .get(`http://localhost:5001/1`)
+      .then((response) => {
+        setMovie(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, [id]);
+
+  const genreNames = movie.genres.map((genre) => genre.name).join(", ");
 
   return (
     <div className="container mx-auto my-8 p-4">
       <div className="bg-white p-6 rounded-lg shadow-lg">
         <div className="flex flex-col md:flex-row">
-          <div className="w-full md:w-1/3">
-            {/* Placeholder for movie image */}
-            <div className="bg-gray-300 h-64 mb-4"></div>
+          <div className="w-full md:w-1/5">
+            <Image
+              className="mb-4"
+              alt="poster"
+              src={`${urlPrefixOriginal}/${movie.poster_path}`}
+              width={300}
+              height={450}
+            />
             <div className="bg-yellow-400 text-xl font-bold p-2 mb-4">
-              {movieData.rating}
+              {movie.vote_average ? movie.vote_average.toFixed(1) : "NR"}
             </div>
-            <p>{movieData.reviewersQuote}</p>
+            {/* <p>{movie.reviewersQuote}</p> */}
             <a
               href="#reviews"
               className="text-indigo-600 hover:text-indigo-800 visited:text-purple-600"
@@ -37,20 +51,18 @@ const MoviePage = () => {
             </a>
           </div>
           <div className="w-full md:w-2/3 md:pl-8">
-            <h1 className="text-4xl font-bold mb-2">{movieData.title}</h1>
+            <h1 className="text-4xl font-bold mb-2">{movie.title}</h1>
             <p className="mb-2">
-              {movieData.genres.join(" | ")} | {movieData.duration} | Released:{" "}
-              {movieData.releaseYear}
+              {genreNames} | {movie.runtime} | Released: {movie.release_date}
             </p>
             <h2 className="text-2xl font-bold mb-2">Overview</h2>
-            <p>{movieData.overview}</p>
+            <p>{movie.overview}</p>
             <p className="font-bold mt-4">
-              Directed by:{" "}
-              <span className="font-normal">{movieData.director}</span>
+              Directed by: <span className="font-normal">movie.director</span>
             </p>
             <p className="font-bold">
               Screenplay by:{" "}
-              <span className="font-normal">{movieData.screenplay}</span>
+              <span className="font-normal">movie.screenplay</span>
             </p>
             <div className="flex mt-4">
               {/* Thumbnails for actors would go here */}
@@ -58,14 +70,14 @@ const MoviePage = () => {
             <div className="mt-4">
               <p>
                 Available on:
-                {movieData.streamingServices.map((service) => (
+                {/* {movie.streamingServices.map((service) => (
                   <span
                     key={service}
                     className="inline-block bg-black text-white p-1 mx-2"
                   >
                     {service}
                   </span>
-                ))}
+                ))} */}
               </p>
             </div>
           </div>
--- app/types/movie.ts
+++ app/types/movie.ts
@@ -15,7 +15,7 @@ interface Collection {
 // Define interface for production companies
 interface ProductionCompany {
   id: number;
-  logo_path: string;
+  logo_path: string | null;
   name: string;
   origin_country: string;
 }
@@ -33,49 +33,92 @@ interface SpokenLanguage {
   name: string;
 }
 
-export type Movie =
-  | {
-      backdrop_path: string | null;
-      id: number;
-      original_title: string;
-      overview: string;
-      poster_path: string | null;
-      media_type: string;
-      adult: boolean;
-      title: string;
-      original_language: string;
-      genre_ids: number[];
-      popularity: number;
-      release_date: string;
-      video: boolean;
-      vote_average: number;
-      vote_count: number;
-    }
-  | {
-      adult: boolean;
-      backdrop_path: string;
-      belongs_to_collection: Collection | null;
-      budget: number;
-      genres: Genre[];
-      homepage: string;
-      id: number;
-      imdb_id: string;
-      origin_country: string[];
-      original_language: string;
-      original_title: string;
-      overview: string;
-      popularity: number;
-      poster_path: string;
-      production_companies: ProductionCompany[];
-      production_countries: ProductionCountry[];
-      release_date: string;
-      revenue: number;
-      runtime: number;
-      spoken_languages: SpokenLanguage[];
-      status: string;
-      tagline: string;
-      title: string;
-      video: boolean;
-      vote_average: number;
-      vote_count: number;
-    };
+// export type Movie =
+//   | {
+//       backdrop_path: string | null;
+//       id: number;
+//       original_title: string;
+//       overview: string;
+//       poster_path: string | null;
+//       media_type: string;
+//       adult: boolean;
+//       title: string;
+//       original_language: string;
+//       genre_ids: number[];
+//       popularity: number;
+//       release_date: string;
+//       video: boolean;
+//       vote_average: number;
+//       vote_count: number;
+//     }
+//   | {
+//       adult: boolean;
+//       backdrop_path: string;
+//       belongs_to_collection: Collection | null;
+//       budget: number;
+//       genres: Genre[];
+//       homepage: string;
+//       id: number;
+//       imdb_id: string;
+//       origin_country: string[];
+//       original_language: string;
+//       original_title: string;
+//       overview: string;
+//       popularity: number;
+//       poster_path: string;
+//       production_companies: ProductionCompany[];
+//       production_countries: ProductionCountry[];
+//       release_date: string;
+//       revenue: number;
+//       runtime: number;
+//       spoken_languages: SpokenLanguage[];
+//       status: string;
+//       tagline: string;
+//       title: string;
+//       video: boolean;
+//       vote_average: number;
+//       vote_count: number;
+//     };
+
+// Base properties common to both types of Movie
+interface BaseMovie {
+  adult: boolean;
+  backdrop_path: string | null;
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+// Specific properties for a general listing (simpler API response)
+export interface SimpleMovie extends BaseMovie {
+  media_type: string;
+  genre_ids: number[];
+}
+
+// Detailed properties for a complete movie object (detailed API response)
+export interface DetailedMovie extends BaseMovie {
+  belongs_to_collection: Collection | null;
+  budget: number;
+  genres: Genre[];
+  homepage?: string;
+  imdb_id?: string;
+  origin_country: string[];
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+}
+
+// Union type combining both simple and detailed movie types
+export type Movie = SimpleMovie | DetailedMovie;
