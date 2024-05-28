6cdaffbbb54dc3ec24bb479bde65db3acf2b2db5
Omer Arad <omerar@wix.com>
2024-05-28 20:29:22 +0300

    trending movies without comments

--- app/components/home/trendingMoviesSection.tsx
+++ app/components/home/trendingMoviesSection.tsx
@@ -1,25 +1,9 @@
 import MovieCard from "../movies/movieCard";
 import TrendingSectionSkeleton from "./trendingSectionSkeleton";
-import axios from "axios";
-import { TrendingMovies } from "@/app/types/trending";
-import trending from "@/app/data/trendingMovies.json";
 import { getTrendingMovies } from "@/app/api/movie/movieServices";
 
 const TrendingMoviesSection = async () => {
-  // const [trendingMovies, setTrendingMovies] = React.useState<TrendingMovies>(
-  //   trending.trendingMovies
-  // );
-
   const trendingMovies = await getTrendingMovies();
-
-  // React.useEffect(() => {
-  //   axios
-  //     .get("/api/movie/trending")
-  //     .then((response) => {
-  //       setTrendingMovies(response.data);
-  //     })
-  //     .catch((error) => console.log(error));
-  // }, []);
 
   return <TrendingSectionSkeleton data={trendingMovies} Card={MovieCard} />;
 };
