3eeceeee9d86bd00a69bbf513051ccf80ebc8d82
Omer Arad <omerar@wix.com>
2024-05-29 10:03:36 +0300

    display cast list in movie page

--- app/api/movie/movieServices.ts
+++ app/api/movie/movieServices.ts
@@ -1,10 +1,11 @@
 import { DetailedMovie, TrendingMovie } from "@/app/types/movie";
 import { apiClient } from "../auth/auth";
+import { CastMember } from "@/app/types/cast";
 
-export const getMovie = async (movieId: string): Promise<DetailedMovie> => {
+export const getMovie = async (movie_id: string): Promise<DetailedMovie> => {
   "use server";
   try {
-    const res = await apiClient.get(`/api/movie/${movieId}`);
+    const res = await apiClient.get(`/api/movie/${movie_id}`);
     return res.data;
   } catch (error) {
     throw error;
@@ -15,6 +16,26 @@ export const getTrendingMovies = async (): Promise<TrendingMovie[]> => {
   "use server";
   try {
     const res = await apiClient.get(`/api/movie/trending`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getMovies = async (): Promise<DetailedMovie[]> => {
+  "use server";
+  try {
+    const res = await apiClient.get(`/api/movie/all`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getMovieCast = async (movie_id: string): Promise<CastMember[]> => {
+  "use server";
+  try {
+    const res = await apiClient.get(`/api/movie/cast/${movie_id}`);
     return res.data;
   } catch (error) {
     throw error;
