be56dc4f64b3bd9d7c63f22646675216479b6bdd
Omer Arad <omerar@wix.com>
2024-06-25 16:33:41 +0300

    add and remove movie to and from watchlist

--- app/api/watchlist/watchlistServices.ts
+++ app/api/watchlist/watchlistServices.ts
@@ -1,14 +1,15 @@
 import { JWT } from "next-auth/jwt";
 import { Key } from "react";
 import { apiClient } from "../auth/auth";
+import { Watchlist } from "@/app/types/watchlist";
 
-export type Watchlist = {
-  id: Key | null | undefined;
-  user_id: number;
-  name: string;
-  description: string;
-  token: JWT;
-};
+// export type Watchlist = {
+//   id: Key | null | undefined;
+//   user_id: number;
+//   name: string;
+//   description: string;
+//   token: JWT;
+// };
 
 // Create a new watchlist
 export const createWatchlist = async (
@@ -27,9 +28,14 @@ export const createWatchlist = async (
 };
 
 // Get a specific watchlist
-export const getWatchlist = async (watchlistId: number) => {
+export const getWatchlist = async (
+  watchlistId?: string | number
+): Promise<Watchlist> => {
   try {
-    const response = await apiClient.get(`/api/watchlists/${watchlistId}`);
+    const url = watchlistId
+      ? `/api/watchlists?watchlist_id=${watchlistId}`
+      : "/api/watchlists";
+    const response = await apiClient.get(url);
     return response.data;
   } catch (error) {
     throw error;
@@ -37,9 +43,42 @@ export const getWatchlist = async (watchlistId: number) => {
 };
 
 // Get all watchlists for a user
-export const getUserWatchlists = async () => {
+export const getUserWatchlists = async (): Promise<Watchlist> => {
   try {
-    const response = await apiClient.get(`/api/users/watchlists`);
+    const response = await apiClient.get(`/api/watchlists/all`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Add a movie/show to the watchlist
+export const addToWatchlist = async (
+  watchlist_id: string,
+  content_id: string,
+  is_movie: boolean
+) => {
+  try {
+    const response = await apiClient.put("/api/watchlists/content", {
+      watchlist_id,
+      content_id,
+      is_movie,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Delete a movie/show from the watchlist
+export const removeFromWatchlist = async (
+  content_id: string,
+  watchlist_id?: string
+) => {
+  try {
+    const response = await apiClient.delete("/api/watchlists/content", {
+      data: { watchlist_id, content_id },
+    });
     return response.data;
   } catch (error) {
     throw error;
