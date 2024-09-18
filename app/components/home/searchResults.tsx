aa28fbe097bbdcb0c2df76fd4aa1ea2632a1025e
Omer Arad <omerar@wix.com>
2024-09-18 13:52:41 +0300

    link search result with relevant page and add fallback photo

--- app/components/home/searchResults.tsx
+++ app/components/home/searchResults.tsx
@@ -1,35 +1,45 @@
 import React from "react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 type SearchResultsProps = {
   results: any[];
 };
 
+const fallbackImage = "/not-found-image.jpg";
+
 const SearchResults: React.FC<SearchResultsProps> = ({ results }) => {
+  const pathname = usePathname();
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {results.map((result) => (
-        <div
+        <Link
           key={result.id}
-          className="flex flex-col items-center bg-gray-800 p-4 rounded-lg"
+          href={`${pathname}movies/${result.id}`}
+          className="flex flex-col items-center bg-gray-800 p-4 rounded-lg cursor-pointer hover:bg-gray-700 transition duration-200"
         >
           <Image
-            src={`https://image.tmdb.org/t/p/w200${result.poster_path}`}
-            alt={result.title || result.name}
+            src={
+              result.poster_path
+                ? `https://image.tmdb.org/t/p/w200${result.poster_path}`
+                : fallbackImage
+            }
+            alt={result.poster_path || result.title || result.name}
             width={200}
             height={300}
             className="rounded-md"
           />
-          <h3 className="text-lg font-semibold mt-2 text-white">
+          <h3 className="text-lg font-semibold mt-2 text-white text-center">
             {result.title || result.name}
           </h3>
-          <p className="text-sm text-gray-400">
+          <p className="text-sm text-gray-400 text-center">
             {result.release_date || result.first_air_date}
           </p>
-          <p className="text-sm text-gray-400">
+          <p className="text-sm text-gray-400 text-center">
             Rating: {result.vote_average.toFixed(1)} / 10
           </p>
-        </div>
+        </Link>
       ))}
     </div>
   );
