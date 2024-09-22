05d032072ef103b8bba847096815fe89e9def7e3
Omer Arad <omerar@wix.com>
2024-09-22 13:10:33 +0300

    two options for display

--- app/components/actors/actorDetails.tsx
+++ app/components/actors/actorDetails.tsx
@@ -1,8 +1,32 @@
-import { ActorDetailsProps } from "@/app/types/actor";
+import { ActorDetailsProps, MediaAppearance } from "@/app/types/actor";
 import Image from "next/image";
 import { TextExpander } from "../textExpander";
+import MovieCard from "../movies/movieCard";
+import TvShowCard from "../tvSeries/tvShowCard";
+import TrendingSectionSkeleton from "../home/trendingSectionSkeleton";
+import Link from "next/link";
 
-const ActorDetails = ({ actor }: ActorDetailsProps) => {
+const pathname = "http://localhost:3000";
+
+const ActorDetails = ({ actor, mediaAppearances }: ActorDetailsProps) => {
+  const getMediaCard = (media: MediaAppearance, index: number) =>
+    media.media_kind === "movie" ? (
+      <MovieCard key={index} movie={media} />
+    ) : (
+      <TvShowCard key={index} tvShow={media} />
+    );
+
+  const getMediaLink = (media: MediaAppearance) => {
+    const link = media.media_kind === "movie" ? `movies` : `tv`;
+    return `${pathname}/${link}/${media.id}`;
+  };
+
+  const getCastAppearances = () =>
+    mediaAppearances.cast.map((media, index) => getMediaCard(media, index));
+
+  const getCrewAppearances = () =>
+    mediaAppearances.crew.map((media, index) => getMediaCard(media, index));
+
   return (
     <div className="container mx-auto p-4">
       <div className="flex flex-col items-center sm:flex-row">
@@ -15,14 +39,14 @@ const ActorDetails = ({ actor }: ActorDetailsProps) => {
             className="rounded-md"
           />
         </div>
-        <div className="mt-4 sm:mt-0 sm:ml-6 overflow-hidden ">
+        <div className="mt-4 sm:mt-0 sm:ml-6 overflow-hidden">
           <h1 className="text-3xl font-bold">{actor.name}</h1>
           <p className="text-sm text-gray-500 mt-2">
             {actor.known_for_department}
           </p>
-          {/* Use the TextExpander for the biography */}
+
           <TextExpander text={actor.biography} initialClampLines={5} />
-          {/* <p className="mt-2 text-lg truncate">{actor.biography}</p> */}
+
           <div className="mt-4">
             <p>
               <strong>Birthday:</strong> {actor.birthday}
@@ -62,6 +86,82 @@ const ActorDetails = ({ actor }: ActorDetailsProps) => {
             </p>
           </div>
         </div>
+      </div>
+
+      <div className="mt-8">
+        <h2 className="text-2xl font-bold mb-4">Media Appearances</h2>
+
+        {mediaAppearances.cast.length > 0 && (
+          <TrendingSectionSkeleton
+            sectionTitle="Cast"
+            Trending={getCastAppearances}
+          />
+        )}
+        {mediaAppearances.crew.length > 0 && (
+          <TrendingSectionSkeleton
+            sectionTitle="Crew"
+            Trending={getCrewAppearances}
+          />
+        )}
+
+        {mediaAppearances.cast.length > 0 && (
+          <div className="mb-8">
+            <h3 className="text-xl font-semibold mb-2">Cast</h3>
+            <div className="flex gap-4 overflow-x-auto pb-3">
+              {mediaAppearances.cast.map((media) => (
+                <Link
+                  key={media.id}
+                  className="bg-gray-800 p-4 rounded-lg max-h-[calc(22rem)] min-w-52 w-52 hover:bg-gray-700 cursor-pointer overflow-y-hidden flex flex-col"
+                  href={getMediaLink(media)}
+                >
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w200${media.poster_path}`}
+                    alt={media.name || media.title!}
+                    width={150}
+                    height={225}
+                    className="rounded-md self-center"
+                  />
+                  <h4 className="text-lg font-semibold mt-2 text-white">
+                    {media.name || media.title}
+                  </h4>
+                  <p className="text-sm text-gray-400">
+                    {media.character ? `as ${media.character}` : ""}
+                  </p>
+                  <p className="text-sm text-gray-400">
+                    {media.first_air_date || media.release_date}
+                  </p>
+                </Link>
+              ))}
+            </div>
+          </div>
+        )}
+
+        {mediaAppearances.crew.length > 0 && (
+          <div>
+            <h3 className="text-xl font-semibold mb-2">Crew</h3>
+            <div className="flex gap-4 overflow-x-auto pb-3">
+              {mediaAppearances.crew.map((media) => (
+                <div
+                  key={media.id}
+                  className="bg-gray-800 p-4 rounded-lg max-h-[calc(22rem)] w-52 min-w-52 hover:bg-gray-700 cursor-pointer overflow-y-hidden flex flex-col"
+                >
+                  <Image
+                    src={`https://image.tmdb.org/t/p/w200${media.poster_path}`}
+                    alt={media.title!}
+                    width={150}
+                    height={225}
+                    className="rounded-md self-center"
+                  />
+                  <h4 className="text-lg font-semibold mt-2 text-white">
+                    {media.title}
+                  </h4>
+                  <p className="text-sm text-gray-400">{media.job}</p>
+                  <p className="text-sm text-gray-400">{media.release_date}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
