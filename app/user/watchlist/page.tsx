7e4d221c9fa8353c0ab21db255a0bd89d9160e46
Omer Arad <omerar@wix.com>
2024-10-03 14:32:48 +0300

    use session

--- app/user/watchlist/page.tsx
+++ app/user/watchlist/page.tsx
@@ -1,9 +1,12 @@
+import { SessionProvider } from "next-auth/react";
 import MainWatchlist from "./mainWatchlist";
 
 const Watchlist = () => {
   return (
     <div className="container mx-auto my-2 py-4 px-16">
-      <MainWatchlist />
+      <SessionProvider>
+        <MainWatchlist />
+      </SessionProvider>
     </div>
   );
 };
