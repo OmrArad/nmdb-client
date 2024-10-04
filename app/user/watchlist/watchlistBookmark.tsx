736b2a96f45a0a18d6e313589b1aeb5ee5090b79
Omer Arad <omerar@wix.com>
2024-10-05 02:57:42 +0300

    prevent default + gray bookmark

--- app/user/watchlist/watchlistBookmark.tsx
+++ app/user/watchlist/watchlistBookmark.tsx
@@ -5,12 +5,14 @@ interface WatchlistBookmarkProps {
   isInWatchlist: boolean;
   handleAdd: () => void;
   handleRemove: () => void;
+  shouldShowTooltip?: boolean;
 }
 
 const WatchlistBookmark = ({
   isInWatchlist,
   handleAdd,
   handleRemove,
+  shouldShowTooltip = false,
 }: WatchlistBookmarkProps) => {
   const [showTooltip, setShowTooltip] = useState(false);
 
@@ -22,6 +24,17 @@ const WatchlistBookmark = ({
     setShowTooltip(false);
   };
 
+  const handleClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (isInWatchlist) {
+      handleRemove();
+    } else {
+      handleAdd();
+    }
+  };
+
   const Tooltip = () => {
     return (
       <>
@@ -31,7 +44,7 @@ const WatchlistBookmark = ({
               <FaCheck />
             </div>
           ) : (
-            <div className="absolute border border-yellow-500 bg-yellow-200 text-black text-xs rounded py-1 px-2 ml-1 transform -translate-y-2/3 bg-opacity-90 ">
+            <div className="absolute border border-yellow-500 bg-yellow-100 text-black text-xs rounded py-1 px-2 ml-1 transform -translate-y-2/3 bg-opacity-90 ">
               <FaPlus />
             </div>
           ))}
@@ -42,18 +55,25 @@ const WatchlistBookmark = ({
   return (
     <>
       <div
-        className="absolute -top-0.5 left-0 text-yellow-500 cursor-pointer transition-transform transform scale-y-150 duration-300 ease-in-out hover:translate-y-1"
+        className="absolute top-0 left-0 text-yellow-400 cursor-pointer transition-transform transform scale-y-150 duration-300 ease-in-out "
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
+        onClick={handleClick}
       >
         {isInWatchlist ? (
-          <FaBookmark size={18} onClick={handleRemove} />
+          <FaBookmark
+            size={18}
+            className="hover:text-gray-400 transition-transform transform duration-300 ease-in-out hover:-translate-y-0.5"
+          />
         ) : (
-          <FaRegBookmark size={18} onClick={handleAdd} />
+          <FaBookmark
+            size={18}
+            className="text-gray-400 hover:text-yellow-400 transition-transform transform duration-300 ease-in-out -translate-y-0.5 hover:translate-y-0.5"
+          />
         )}
       </div>
 
-      <Tooltip />
+      {shouldShowTooltip && <Tooltip />}
     </>
   );
 };
