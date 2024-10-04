605c5510420132fd22b826382399ebaf04e2fec1
Omer Arad <omerar@wix.com>
2024-10-04 23:21:10 +0300

    added tooltip

--- app/user/watchlist/watchlistBookmark.tsx
+++ app/user/watchlist/watchlistBookmark.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaBookmark, FaRegBookmark } from "react-icons/fa";
+import { FaBookmark, FaRegBookmark, FaCheck, FaPlus } from "react-icons/fa";
 
 interface WatchlistBookmarkProps {
   isInWatchlist: boolean; // Determines whether the item is in the watchlist
@@ -12,34 +12,44 @@ const WatchlistBookmark = ({
   handleAdd,
   handleRemove,
 }: WatchlistBookmarkProps) => {
-  const [isHovered, setisHovered] = useState(false);
+  const [showTooltip, setShowTooltip] = useState(false);
 
   const handleMouseEnter = () => {
-    setisHovered(true);
+    setShowTooltip(true);
   };
 
   const handleMouseLeave = () => {
-    setisHovered(false);
+    setShowTooltip(false);
+  };
+
+  const Tooltip = () => {
+    return (
+      <>
+        {showTooltip && (
+          <div className="absolute bg-yellow-500 text-white text-xs rounded py-1 px-2 ml-2  transform -translate-y-3/4 whitespace-nowrap">
+            {isInWatchlist ? <FaCheck /> : <FaPlus />}
+          </div>
+        )}
+      </>
+    );
   };
 
   return (
-    <div
-      className="absolute -top-0.5 left-0 text-yellow-500 cursor-pointer "
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      {isInWatchlist ? (
-        isHovered ? (
-          <FaRegBookmark size={18} onClick={handleRemove} />
+    <>
+      <div
+        className="absolute -top-0.5 left-0 text-yellow-500 cursor-pointer transition-transform transform hover:scale-y-150 duration-300 ease-in-out"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        {isInWatchlist ? (
+          <FaBookmark size={18} onClick={handleRemove} />
         ) : (
-          <FaBookmark size={18} onClick={handleAdd} />
-        )
-      ) : isHovered ? (
-        <FaBookmark size={18} onClick={handleAdd} />
-      ) : (
-        <FaRegBookmark size={18} onClick={handleRemove} />
-      )}
-    </div>
+          <FaRegBookmark size={18} onClick={handleAdd} />
+        )}
+      </div>
+
+      <Tooltip />
+    </>
   );
 };
 
