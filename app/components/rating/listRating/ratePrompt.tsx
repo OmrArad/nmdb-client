b08f4394d20a1308490d3e1dee981b8816d6b20c
Omer Arad <omerar@wix.com>
2024-10-06 03:25:19 +0300

    ability to hide text in ratePrompt

--- app/components/rating/listRating/ratePrompt.tsx
+++ app/components/rating/listRating/ratePrompt.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { FaRegStar } from "react-icons/fa";
 
-const RatePrompt: React.FC = () => {
+type RatePromptProps = { showText: boolean };
+
+const RatePrompt: React.FC<RatePromptProps> = ({ showText }) => {
   return (
     <div className="flex justify-center items-center">
-      <span className="font-bold">Rate</span>
+      {showText && <span className="font-bold">Rate</span>}
       <div className="text-blue-400 rounded-full p-1">
         <FaRegStar size={15} />
       </div>
