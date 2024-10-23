4aaf59fd4fdbf5a6e219f465353601eebc5c9ca4
Omer Arad <omerar@wix.com>
2024-10-23 14:05:52 +0300

    add trailer as popup in watchlist

--- app/components/watchlist/watchlistItem.tsx
+++ app/components/watchlist/watchlistItem.tsx
@@ -4,13 +4,15 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { IWatchlistItem } from "@/app/types/watchlist";
 import { useWatchlist } from "@/app/context/watchlistContext";
-import { useRouter } from "next/navigation"; // For programmatic navigation
+import { useRouter } from "next/navigation";
 import WatchlistBookmark from "./watchlistBookmark";
 import Ratings from "@/app/components/rating/listRating/ratings";
+import { FaYoutube } from "react-icons/fa";
 
 const WatchlistItem = ({ media }: { media: IWatchlistItem }) => {
   const { watchlist, updateWatchlist } = useWatchlist();
   const [isInWatchlist, setIsInWatchlist] = useState(true);
+  const [isVideoOpen, setIsVideoOpen] = useState(false); // State to manage popup
   const router = useRouter();
 
   const {
@@ -31,13 +33,16 @@ const WatchlistItem = ({ media }: { media: IWatchlistItem }) => {
     router.push(navLink);
   };
 
+  const openVideoPopup = () => setIsVideoOpen(true);
+  const closeVideoPopup = () => setIsVideoOpen(false);
+
   return (
     <div className="bg-gray-100 border border-gray-300 rounded-xl overflow-hidden shadow-lg mb-4 relative">
       <div className="md:flex items-start p-4 relative">
         <Image
-          src={poster_path ? poster_path : "/images/no-image-available.png"}
+          src={poster_path || "/images/no-image-available.png"}
           alt={title}
-          className="w-24 h-36 mr-4 cursor-pointer transition-transform transform  rounded-lg hover:brightness-105 hover:scale-105 duration-300 ease-in-out"
+          className="w-24 h-36 mr-4 cursor-pointer transition-transform transform rounded-lg hover:brightness-105 hover:scale-105 duration-300 ease-in-out"
           width={96}
           height={144}
           onClick={handleNavigate}
@@ -51,25 +56,13 @@ const WatchlistItem = ({ media }: { media: IWatchlistItem }) => {
           shouldShowIcon={false}
         />
 
-        <div>
-          {/* YouTube video */}
-        <div className="mt-4">
-          <iframe
-            width="300"
-            height="169"
-            src={`${urlPrefixYoutube}/${media.video_links}`}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen={true}
-          ></iframe>
-        </div>
-          <div className="flex-col justify-between">
+        <div className="flex-col">
+          <div className="flex-col justify-between mt-4">
             <div className="flex justify-between items-center">
               <div className="flex flex-col">
                 <Link
                   href={navLink}
-                  className="text-xl font-bold transition-transform transform hover:scale-105 "
+                  className="text-xl font-bold transition-transform transform hover:scale-105"
                 >
                   {title}
                 </Link>
@@ -78,9 +71,42 @@ const WatchlistItem = ({ media }: { media: IWatchlistItem }) => {
               <Ratings isMovie={true} media={media} />
             </div>
             <p className="text-sm mt-2">{overview}</p>
+            {/* Trailer button to open popup */}
+            {video_links?.length > 0 && (
+              <button
+                onClick={openVideoPopup}
+                className="flex gap-2 mt-2 text-red-500 text-sm font-bold px-0.5 py-0.5 rounded-lg hover:scale-110 transition"
+              >
+                <FaYoutube size={20} color="red" />
+                Watch Trailer
+              </button>
+            )}
           </div>
         </div>
       </div>
+
+      {/* Popup for YouTube Video */}
+      {isVideoOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50">
+          <div className="relative w-[90%] max-w-2xl">
+            <button
+              onClick={closeVideoPopup}
+              className="absolute -top-10 -right-0.5 text-white text-xl rounded-full p-0.5 hover:scale-150"
+            >
+              &times;
+            </button>
+            <iframe
+              width="100%"
+              height="315"
+              src={`${urlPrefixYoutube}/${media.video_links}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
