0fbb985da945d655084a9cea5fe1734f7f3513c1
Omer Arad <omerar@wix.com>
2024-10-23 13:22:51 +0300

    small fix

--- app/components/user/userAvatar.tsx
+++ app/components/user/userAvatar.tsx
@@ -2,15 +2,24 @@
 import React from "react";
 import { Session } from "next-auth";
 import UserDropdown from "./userDropdown";
+import { UserData } from "@/app/types/auth";
 
 const UserAvatar = ({
   handleLogout,
   session,
+  userData,
 }: {
   handleLogout: () => void;
   session: Session;
+  userData: UserData;
 }) => {
-  return <UserDropdown onLogoutClick={handleLogout} _session={session} userData={null}/>;
+  return (
+    <UserDropdown
+      onLogoutClick={handleLogout}
+      _session={session}
+      userData={userData}
+    />
+  );
 };
 
 export default UserAvatar;
