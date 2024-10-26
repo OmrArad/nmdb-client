f3e946127f26a0ab298623b953bdc8b2a2ee2b24
Me2303 <63667515+Me2303@users.noreply.github.com>
2024-10-27 00:21:29 +0300

    Added spinner to recommendations loading, fixed ratings placement in the list, changed buttons, added content type icons and text

--- app/user/recommendations/recommendedItem.tsx
+++ app/user/recommendations/recommendedItem.tsx
@@ -9,7 +9,7 @@ import { isMediaInWatchlist } from "@/app/utils/watchlistUtils";
 import { IRecommendedItem } from "@/app/types/recommendations";
 import WatchlistBookmark from "@/app/components/watchlist/watchlistBookmark";
 import TrailerButtonClientWrapper from "@/app/components/trailer/trailerButtonClientWrapper";
-import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
+import { FaThumbsUp, FaThumbsDown, FaFilm, FaTv } from "react-icons/fa";
 import { sendRecommendationFeedback } from "@/app/api/recommendations/recommendationFeedbackServices";
 
 const RecommendedItem = ({
@@ -29,7 +29,7 @@ const RecommendedItem = ({
     is_movie,
   } = media;
 
-  const [feedbackGiven, setFeedbackGiven] = useState<boolean | null>(null); // Track like/dislike state
+  const [feedbackGiven, setFeedbackGiven] = useState<boolean | null>(null);
   const { watchlist, updateWatchlist } = useWatchlist();
   const [isInWatchlist, setIsInWatchlist] = useState(
     shouldCheckisInWatchlistStatus
@@ -53,7 +53,7 @@ const RecommendedItem = ({
 
       console.log("recommendationWatchlist: ", recommendationWatchlist);
 
-      setFeedbackGiven(isLiked); // Update UI to reflect feedback
+      setFeedbackGiven(isLiked);
     } catch (error) {
       console.error("Error sending feedback", error);
     }
@@ -80,53 +80,57 @@ const RecommendedItem = ({
           isMovie={is_movie}
         />
 
-        <div className="flex-col">
-          <div className="flex-col justify-between mt-4">
-            <div className="flex justify-between items-center">
-              <div className="flex flex-col">
-                <Link
-                  href={navLink}
-                  className="text-xl font-bold transition-transform transform hover:scale-105"
-                >
-                  {title}
-                </Link>
-                <p className="text-gray-400">{release_date}</p>
-              </div>
-              <Ratings isMovie={is_movie} media={media} />
-            </div>
-            <p className="text-sm mt-2">{overview}</p>
-
-            {video_links?.length > 0 && (
-              <TrailerButtonClientWrapper videoKey={media.video_links} />
-            )}
-
-            <div className="flex gap-2 mt-4">
-              <button
-                className={`flex items-center gap-1 px-4 py-2 rounded-lg transition hover:scale-105 hover:bg-green-500 hover:bg-opacity-50 ${
-                  feedbackGiven === true
-                    ? "bg-green-500 text-white"
-                    : "bg-gray-200"
-                }`}
-                onClick={() => handleFeedback(true)}
-                disabled={feedbackGiven !== null}
+        <div className="flex-col w-full relative">
+          <div className="absolute top-0 right-0">
+            <Ratings isMovie={is_movie} media={media} />
+          </div>
+          <div className="flex justify-between items-center mt-4">
+            <div className="flex flex-col">
+              <span className="text-sm font-semibold text-gray-500 flex items-center gap-1">
+                {is_movie ? <FaFilm /> : <FaTv />} {/* Icon */}
+                {is_movie ? "Movie" : "TV Show"}
+              </span>
+              <Link
+                href={navLink}
+                className="text-xl font-bold transition-transform transform hover:scale-105"
               >
-                <FaThumbsUp />
-                Like
-              </button>
-
-              <button
-                className={`flex items-center gap-1 px-4 py-2 rounded-lg transition hover:scale-105 hover:bg-red-500 hover:bg-opacity-50 ${
-                  feedbackGiven === false
-                    ? "bg-red-500 text-white"
-                    : "bg-gray-200"
-                }`}
-                onClick={() => handleFeedback(false)}
-                disabled={feedbackGiven !== null}
-              >
-                <FaThumbsDown />
-                Dislike
-              </button>
+                {title}
+              </Link>
+              <p className="text-gray-400">{release_date}</p>
             </div>
+          </div>
+          <p className="text-sm mt-2">{overview}</p>
+
+          {video_links?.length > 0 && (
+            <TrailerButtonClientWrapper videoKey={media.video_links} />
+          )}
+
+          <div className="flex gap-2 mt-4">
+            <button
+              className={`flex items-center gap-1 px-4 py-2 rounded-lg transition hover:scale-105 hover:bg-green-500 hover:bg-opacity-50 ${
+                feedbackGiven === true
+                  ? "bg-green-500 text-white"
+                  : "bg-gray-200"
+              }`}
+              onClick={() => handleFeedback(true)}
+              disabled={feedbackGiven !== null}
+            >
+              <FaThumbsUp />
+              Like
+            </button>
+
+            <button
+              className={`flex items-center gap-1 px-4 py-2 rounded-lg transition hover:scale-105 hover:bg-red-500 hover:bg-opacity-50 ${
+                feedbackGiven === false
+                  ? "bg-red-500 text-white"
+                  : "bg-gray-200"
+              }`}
+              onClick={() => handleFeedback(false)}
+              disabled={feedbackGiven !== null}
+            >
+              <FaThumbsDown />
+              Dislike
+            </button>
           </div>
         </div>
       </div>
