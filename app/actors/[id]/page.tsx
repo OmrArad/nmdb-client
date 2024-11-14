c907f892969d74d54a3d758778eb32f148c0c904
Me2303 <63667515+Me2303@users.noreply.github.com>
2024-11-14 16:47:08 +0200

    Fixed type in actor/[id]/page

--- app/actors/[id]/page.tsx
+++ app/actors/[id]/page.tsx
@@ -10,7 +10,7 @@ type ActorPageProps = {
   };
 };
 
-export default async function ActorPage({ params: { id } }: ActorPageProps) {
+export default async function ActorPage({ params: { id } }: { params: { id: string } }) {
   const actor = await getActorDetails(id);
   const mediaAppearances = await fetchMediaAppearances(id);
 
@@ -18,7 +18,6 @@ export default async function ActorPage({ params: { id } }: ActorPageProps) {
     <div className="container mx-auto my-8 p-4">
       <div className="bg-white p-6 rounded-lg shadow-lg">
         <ActorDetails actor={actor} mediaAppearances={mediaAppearances} />
-        {/* Here you might include additional sections such as a forum link, etc */}
       </div>
     </div>
   );
